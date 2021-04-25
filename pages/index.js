1515e9823f593d79c7965923dfbb06bbc493998e
Andrey Pacheco <andreypacheco@yandex.ru>
2021-04-25 13:40:15 +0300

    fix: исправить ошибки

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,28 @@ let popupClose = document.querySelector('.popup__close-btn');
 let popupContainer = document.querySelector('.popup__container');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
-let inputName = document.querySelector('.popup__item_type-name');
-let inputAbout = document.querySelector('.popup__item_type-about');
+let inputName = document.querySelector('.popup__item_type_name');
+let inputAbout = document.querySelector('.popup__item_type_about');
 
 
-function renderPopup () {
-
+function openPopup () {
   popup.classList.toggle('popup_opened');
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
-profileEditBtn.addEventListener('click', renderPopup);
-popupClose.addEventListener('click', renderPopup);
+
+function closePopup () {
+  popup.classList.toggle('popup_opened');
+}
 
 function sabmitProfile (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  popup.classList.toggle('popup_opened');
+  closePopup();
 }
 
+profileEditBtn.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
 popupContainer.addEventListener('submit', sabmitProfile);
 
